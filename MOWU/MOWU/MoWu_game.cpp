06feb641b4e6d40198842d5b0701d50515bedacc
涂山月 <LLQsky@live.cn>
2018-01-06 13:04:24 +0800

    201801061303 fix two bugs in Mowu_bullet.h

--- MOWU/MOWU/MoWu_game.cpp
+++ MOWU/MOWU/MoWu_game.cpp
@@ -12,12 +12,23 @@
 
 int main()
 {
-	initgraph(SCREEN_WIDTH,SCREEN_LENGTH);	//初始化UI界面
-	Bullet a = { 100,100,10,0.9,trail_sin,WHITE};
+	initgraph(SCREEN_WIDTH, SCREEN_LENGTH);	//初始化UI界面
+	Bullet a0(320, 240, 5, 0, trail_sin, WHITE);
+	Bullet a1(320, 240, 5, 3.14159 / 2, trail_sin, WHITE);
+	Bullet a2(320, 240, 5, 3.14159, trail_sin, WHITE);
+	Bullet a3(320, 240, 5, 3.14159 * 1.5, trail_sin, WHITE);
+	Bullet a[21];
+	for (int i = 0; i < 21; i++)
+	{
+		a[i].initialization(320, 240, 5, i / 20.0 * 2 * Pi, trail_sin, WHITE);
+	}
 	while (true)
 	{
 		BeginBatchDraw();
-		a.fresh();
+		for (int i = 0; i < 21; i++)
+		{
+			a[i].fresh();
+		}
 		FlushBatchDraw();
 		Sleep(20);
 	}
--- MOWU/MOWU/Mowu_bullet.h
+++ MOWU/MOWU/Mowu_bullet.h
@@ -7,6 +7,8 @@ class Bullet
 private:
 	int x;			//初始位置:X
 	int y;			//初始位置:y
+	int nowx = 0;	//坐标位置:x'
+	int nowy = 0;	//坐标位置:y'
 	int r;			//弹幕半径
 	double angle;	//绘制角度
 	void(*trail)(int, int&, int&);//路径样式
@@ -17,10 +19,29 @@ public:
 		const int x_ = 0,
 		const int y_ = 0,
 		const int r_ = 10,
-		const double angle_ = 90,
+		const double angle_ = 0,
 		void(*trail_)(int, int&, int&) = trail_line,
 		int color_ = WHITE
 	)
+
+	{
+		x = x_;
+		y = y_;
+		r = r_;
+		angle = angle_;
+		trail = trail_;
+		color = color_;
+		time.start();
+	}
+	void initialization(
+		const int x_ = 0,
+		const int y_ = 0,
+		const int r_ = 10,
+		const double angle_ = 0,
+		void(*trail_)(int, int&, int&) = trail_line,
+		int color_ = WHITE
+	)
+
 	{
 		x = x_;
 		y = y_;
@@ -32,8 +53,6 @@ public:
 	}
 	void fresh()
 	{
-		static int nowx = 0;
-		static int nowy = 0;
 		setlinecolor(BLACK);
 		setfillcolor(BLACK);
 		fillcircle(nowx, nowy, r);

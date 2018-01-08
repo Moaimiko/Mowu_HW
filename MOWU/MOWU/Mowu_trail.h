52446a599b008b5e5741fca2076681f1434c5c31
Hakurei Reimu <24761242+Hakurei-Reimu-SkylarkStudio@users.noreply.github.com>
2018-01-08 14:51:09 +0800

    201801081453 注释Mowu_trail.h

--- MOWU/MOWU/Mowu_trail.h
+++ MOWU/MOWU/Mowu_trail.h
@@ -1,42 +1,43 @@
 //Trail.h
-//��Ļ�켣����
+//弹幕轨迹函数
 #pragma once
-//ֱ�߹켣
+//直线轨迹
 void trail_line(const int t,int& x,int& y)
 {
 	x = 0.1*t;
 	y = 0;
 }
-//���ι켣
+//蛇形轨迹
 void trail_sin(const int t, int& x, int& y)
 {
 	x = 0.1*t;
 	y = 20*sin(t / 100.0);
 }
-//Բ�ι켣(��ʱ��)
+//圆形轨迹(逆时针)
 void trail_circle_0(const int t, int &x, int &y)
 {
 	x = 0.1*t*sin(t / 1000.0);
 	y = 0.1*t*cos(t / 1000.0);
 }
-//Բ�ι켣(˳ʱ��)
+//圆形轨迹(顺时针)
 void trail_circle_1(const int t, int &x, int &y)
 {
 	x = 0.1*t*sin(t / 1000.0);
 	y = -0.1*t*cos(t / 1000.0);
 }
-//Բ��Ѳ�ع켣(��ʱ��)
+//圆形巡回轨迹(逆时针)
 void trail_circle_2(const int t, int &x, int &y)
 {
 	x = 0.05*t*sin(t / 1000.0);
 	y = 0.1*t*cos(t / 1000.0);
 }
-//Բ��Ѳ�ع켣(˳ʱ��)
+//圆形巡回轨迹(顺时针)
 void trail_circle_3(const int t, int &x, int &y)
 {
 	x = 0.1*t*sin(t / 1000.0);
 	y = -0.05*t*cos(t / 1000.0);
 }
+//静止弹幕（测试用）
 void trail_stop(const int t, int &x, int &y)
 {
 	x = 0;
